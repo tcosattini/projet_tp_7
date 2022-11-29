fb55df212f44924d1af12e0973a3e983ebbecbd9
bishem <aminrsaidali@gmail.com>
2022-11-29 16:07:01 +0100

    fixed project referencing by refactoring old name

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projet_fil_rouge_api project.
+WSGI config for api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet_fil_rouge_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
