e7d96594679bf2df24c3e523dfa0e715e01f7a6b
Brice Bayard <brice.bayard@gmail.com>
2022-11-30 17:19:44 +0100

    ajout du panel admin sur l'url index/

--- projet_fil_rouge_api/projet_fil_rouge_api/urls.py
+++ projet_fil_rouge_api/projet_fil_rouge_api/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('index', include('administration.urls'))
 ]
