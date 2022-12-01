9c31f772f4abb95761dad594cbdeb5fc919a05dd
Madani Bensikhaled <bensikhaledmadani@gmail.com>
2022-12-01 17:11:50 +0100

    Sauvegarde madani_bensikhaled

--- projet_fil_rouge_api/administration/urls.py
+++ projet_fil_rouge_api/administration/urls.py
@@ -6,10 +6,19 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+
     path('', views.index, name='index'),
     path('page2.html', views.index2, name='index2'),
+    path('page3.html', views.index3, name='index3'),
+
     re_path(r'^ajoutCommune/(?P<communesValue>[a-zA-Z ]+)_(?P<depValue>[0-9]+)_(?P<cpValue>[a-zA-Z0-9 ]+)$', views.ajoutCommune, name='ajoutCommune'),
-    re_path(r'^modificationCommune/(?P<idCommuneValue>[0-9]+)_(?P<communesValue>[a-zA-Z ]+)_(?P<depValue>[0-9]+)_(?P<cpValue>[a-zA-Z0-9 ]+)$', views.modificationCommune, name='modificationCommune'),
+    re_path(r'^modificationCommune/(?P<idCommuneValue>[0-9]+)_(?P<communesValue>[a-zA-Z ]*)_(?P<depValue>[0-9]*)_(?P<cpValue>[a-zA-Z0-9 ]*)$', views.modificationCommune, name='modificationCommune'),
+
     re_path(r'^ajoutConditionnement/(?P<nomValue>[a-zA-Z0-9 ]+)_(?P<poidsValue>[0-9]+)_(?P<tarifValue>[0-9.]+)$', views.ajoutConditionnement, name='ajoutConditionnement'),
-    re_path(r'^modificationConditionnement/(?P<idConditionnementValue>[0-9]+)_(?P<nomValue>[a-zA-Z0-9 ]+)_(?P<poidsValue>[0-9]+)_(?P<tarifValue>[0-9.]+)$', views.modificationConditionnement, name='modificationConditionnement'),
+    re_path(r'^modificationConditionnement/(?P<idConditionnementValue>[0-9]+)_(?P<nomValue>[a-zA-Z0-9 ]*)_(?P<poidsValue>[0-9]*)_(?P<tarifValue>[0-9.]*)$', views.modificationConditionnement, name='modificationConditionnement'),
+
+    re_path(r'^ajoutObjet/(?P<nomValue>[a-zA-Z0-9 ]+)_(?P<poidsValue>[0-9.]+)_(?P<impValue>[0-9]+)_(?P<publicitaireValue>[0-9.]+)_(?P<conditionnementValue>[0-9]+)_(?P<pointsValue>[0-9]+)$', views.ajoutObjet, name='ajoutObjet'),
+    re_path(r'^modificationObjet/(?P<idObjetValue>[0-9]+)_(?P<nomValue>[a-zA-Z0-9 ]*)_(?P<poidsValue>[0-9.]*)_(?P<impValue>[0-9]*)_(?P<publicitaireValue>[0-9.]*)_(?P<conditionnementValue>[0-9]*)_(?P<pointsValue>[0-9]*)$', views.modificationObjet, name='modificationObjet'),
+
+    re_path(r'^statusObjet/(?P<idObjetValue>[0-9]+)_(?P<statusValue>[0-1])$', views.statusChangeObjet, name='statusChangeObjet'),
 ]
--- projet_fil_rouge_api/administration/views.py
+++ projet_fil_rouge_api/administration/views.py
@@ -16,9 +16,12 @@ def ajoutCommune(request, communesValue, depValue, cpValue):
     return render(request, 'retour.html', )
 
 def modificationCommune(request, idCommuneValue, communesValue, depValue, cpValue):
-    TCommunes.objects.filter(id_commune = idCommuneValue).delete()
-    ajoutCommune = TCommunes(id_commune = idCommuneValue, dep = depValue, cp = cpValue, communes = communesValue)
-    ajoutCommune.save()
+    if (communesValue != ""):
+        TCommunes.objects.filter(id_commune = idCommuneValue).update(communes = communesValue)
+    if (depValue != ""):
+        TCommunes.objects.filter(id_commune = idCommuneValue).update(dep = depValue)
+    if (cpValue != ""):
+        TCommunes.objects.filter(id_commune = idCommuneValue).update(cp = cpValue)
     return render(request, 'retour.html', )
 
 def index2(request):
@@ -34,7 +37,41 @@ def ajoutConditionnement(request, nomValue, poidsValue, tarifValue):
     return render(request, 'retour2.html', )
 
 def modificationConditionnement(request, idConditionnementValue, nomValue, poidsValue, tarifValue):
-    TConditionnement.objects.filter(idcondit = idConditionnementValue).delete()
-    ajoutConditionnement = TConditionnement(idcondit = idConditionnementValue, libcondit = nomValue, poidscondit = poidsValue, prixcond = tarifValue)
-    ajoutConditionnement.save()
+    if (nomValue != ""):
+        TConditionnement.objects.filter(idcondit = idConditionnementValue).update(libcondit = nomValue)
+    if (poidsValue != ""):
+        TConditionnement.objects.filter(idcondit = idConditionnementValue).update(poidscondit = poidsValue)
+    if (tarifValue != ""):
+        TConditionnement.objects.filter(idcondit = idConditionnementValue).update(prixcond = tarifValue)
     return render(request, 'retour2.html', )
+
+def index3(request):
+    data_objet = TObjet.objects.all()
+    context = {
+        "Objets" : data_objet,
+    }
+    return render(request, 'page3.html', context)
+
+def ajoutObjet(request, nomValue, poidsValue, impValue, publicitaireValue, conditionnementValue, pointsValue):
+    ajoutObjet = TObjet(libobj = nomValue, poidsobj = poidsValue, o_imp = impValue, puobj = publicitaireValue, idcondit = conditionnementValue, points = pointsValue, is_active = 1)
+    ajoutObjet.save()
+    return render(request, 'retour3.html', )
+
+def modificationObjet(request, idObjetValue, nomValue, poidsValue, impValue, publicitaireValue, conditionnementValue, pointsValue):
+    if (nomValue != ""):
+        TObjet.objects.filter(codobj = idObjetValue).update(libobj = nomValue)
+    if (poidsValue != ""):
+        TObjet.objects.filter(codobj = idObjetValue).update(poidsobj = poidsValue)
+    if (impValue != ""):
+        TObjet.objects.filter(codobj = idObjetValue).update(o_imp = impValue)
+    if (publicitaireValue != ""):
+        TObjet.objects.filter(codobj = idObjetValue).update(puobj = publicitaireValue)
+    if (conditionnementValue != ""):
+        TObjet.objects.filter(codobj = idObjetValue).update(idcondit = conditionnementValue)
+    if (pointsValue != ""):
+        TObjet.objects.filter(codobj = idObjetValue).update(points = pointsValue)
+    return render(request, 'retour3.html', )
+
+def statusChangeObjet(request, idObjetValue, statusValue):
+    TObjet.objects.filter(codobj = idObjetValue).update(is_active = statusValue)
+    return render(request, 'retour3.html', )
