ed1387e9e52ab2c6f12645502b5ebbc7dc0fef3c
Brice Bayard <brice.bayard@gmail.com>
2022-12-02 10:28:34 +0100

    responsive update on index

--- projet_fil_rouge_api/authentification/views.py
+++ projet_fil_rouge_api/authentification/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from . import forms
 
-# Create your views here.
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def login_page(request):
+    form = forms.LoginForm()
+    message = ''
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                message = f'Bonjour, {user.username}! Vous êtes connecté.'
+                return redirect('/home')
+            else:
+                message = 'Identifiants invalides.'
+    return render(
+        request, 'login/login.html', context={'form': form, 'message': message})
---
+++ projet_fil_rouge_api/gestionColis/service/client.py
@@ -0,0 +1,41 @@
+from ..models import TClient
+from django.core import serializers
+from fastapi import HTTPException
+
+
+def getAll():
+ try:    
+    response =[]
+    serializers.deserialize("json", TClient.objects.all())
+    for obj in TClient.objects.all():
+        response.append(obj)
+    return {"response":response}
+ except: 
+    return {'clients non trouvés'}  
+
+def create(validateObject):
+ try:   
+    newClient =  TClient.objects.create(**validateObject.dict())
+    print(newClient)
+    return {"nouveau client":newClient}
+ except:
+    raise HTTPException(status_code=426, detail="Impossible de créer ce client")
+
+def update(codcli,validateObject):
+ try:
+   findSelectedClient = TClient.objects.get(codcli=codcli)
+   selectedCommande = TClient.objects.filter(codcli=codcli).update(**validateObject.dict())
+   selectedClient = TClient.objects.filter(codcli=codcli).update(**validateObject.dict())
+   print(selectedClient)
+   return {"client modifié avec succés"}
+ except:
+    raise HTTPException(status_code=404, detail="Commande non trouvée")
+
+def delete(codcli):
+ try:
+   findSelectedClient = TClient.objects.get(codcli=codcli)
+   selectedClient = TClient.objects.filter(codcli=codcli).delete()
+   print(selectedClient)
+   return {"client supprimé avec succés"}
+ except:
+    raise HTTPException(status_code=404, detail="Commande non trouvée")
