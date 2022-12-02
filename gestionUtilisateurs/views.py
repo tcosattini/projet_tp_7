1a780d3b9b74a74cf57188ef69a695ab91c9ded5
bishem <aminrsaidali@gmail.com>
2022-12-02 09:16:28 +0100

    pre rebase commit

--- gestionUtilisateurs/views.py
+++ gestionUtilisateurs/views.py
@@ -4,17 +4,11 @@ from django.contrib.auth.decorators import login_required
 from . import models
 
 
-# @login_required(login_url='auth/login')
 def add(request):
     return render(request, template_name='ajout_utilisateur.html')
 
 
-# @login_required(login_url='auth/login')
 def list(request):
-
-    if request.POST.get('toggle_utilisateur'):
-        pass
-
     return render(
         request,
         template_name='liste_utilisateurs.html',
@@ -23,27 +17,24 @@ def list(request):
 
 def toggle_active(request, id: int):
 
-    if request.POST.get('toggle_active'):
-        utilisateur = models.TUtilisateur.objects.get(pk=id)
-        utilisateur.is_active = not utilisateur.is_active
-        utilisateur.save()
+    # if request.POST.get('toggle_active'):
+    utilisateur = models.TUtilisateur.objects.get(pk=id)
+    utilisateur.is_active = not utilisateur.is_active
+    utilisateur.save()
 
     return list(request)
 
 
 def change(request, id: int):
+    return render(
+        request,
+        template_name='change_utilisateur.html',
+        context={'utilisateur': models.TUtilisateur.objects.get(pk=id)})
 
-    if id:
-        return render(
-            request,
-            template_name='liste_utilisateurs.html',
-            context={'changed': False})
+
+def save(request):
 
     return list(request)
-
-
-"""methode used to redirect user to home pae of the module
-"""
 
 
 def home(request):
