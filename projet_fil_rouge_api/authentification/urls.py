9a492b35e8281923673a7d01bfe260322e6d6060
Madani Bensikhaled <bensikhaledmadani@gmail.com>
2022-12-06 14:51:18 +0100

    Suppresion du commit par erreur

--- projet_fil_rouge_api/authentification/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.login_page, name='index' ),
-    path('logout/', views.logout_user, name='logout'),
-]
--- projet_fil_rouge_api/gestionStock/__init__.py
+++
@@ -1,5 +0,0 @@
-# import django
-# import os
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet_fil_rouge_api.settings')
-# django.setup()
