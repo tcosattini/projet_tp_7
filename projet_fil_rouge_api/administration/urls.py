a6de3642e6a96aef9e3099376aea2fef806513b3
Maxime Lidy <maxime.lidy@outlook.fr>
2022-12-06 12:02:56 +0100

    gestion poids

--- projet_fil_rouge_api/administration/urls.py
+++ projet_fil_rouge_api/administration/urls.py
@@ -27,4 +27,13 @@ urlpatterns = [
     re_path(r'^modificationCommune/(?P<idCommuneValue>[0-9]+)_(?P<communesValue>[a-zA-Z ]*)_(?P<depValue>[0-9]*)_(?P<cpValue>[a-zA-Z0-9 ]*)$',
             views.modificationCommune, name='modificationCommune'),
 
+
+
+    #path('', views.index, name='index'),    
+    re_path(r'^pagePoids/(?P<idx>[0-9]+)$', views.pagePoids, name='pagePoids'),
+    re_path(r'^ajoutPoids/(?P<idx>[0-9]+)_(?P<valmin>[0-9.]+)_(?P<valtimbre>[0-9.]+)', views.ajoutPoids, name='ajoutPoids'),
+    re_path(r'^modifierPoids/(?P<idx>[0-9]+)_(?P<id>[0-9.]+)_(?P<valmin>[0-9.]+)_(?P<valtimbre>[0-9.]+)',views.modifierPoids, name='modifierPoids'),
+    re_path(r'^pagePoidsVignettes/(?P<idx>[0-9]+)$', views.pagePoidsVignettes, name='pagePoidsVignettes'),
+    re_path(r'^ajoutPoidsVignettes/(?P<idx>[0-9]+)_(?P<valmin>[0-9.]+)_(?P<valtimbre>[0-9.]+)', views.ajoutPoidsVignettes, name='ajoutPoidsVignettes'),
+    re_path(r'^modifierPoidsVignettes/(?P<idx>[0-9]+)_(?P<id>[0-9.]+)_(?P<valmin>[0-9.]+)_(?P<valtimbre>[0-9.]+)',views.modifierPoidsVignettes, name='modifierPoidsVignettes'),
 ]
--- projet_fil_rouge_api/administration/views.py
+++ projet_fil_rouge_api/administration/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
@@ -110,3 +111,67 @@ def modificationCommune(request, idCommuneValue, communesValue, depValue, cpValu
     if (cpValue != ""):
         TCommunes.objects.filter(id_commune=idCommuneValue).update(cp=cpValue)
     return render(request, 'retour_communes.html', )
+from .models import *
+import math
+
+# Create your views here.
+
+'''def index(request):
+    data_poids = TPoids.objects.all()
+    context = {
+        "Poids": data_poids,
+    }
+    return render(request, 'index.html', context)'''
+
+
+def pagePoids(request, idx):
+    idx = int(idx)
+    t_min = (idx-1)*10
+    t_max = (idx)*10
+    taille = len(TPoids.objects.all())
+    idx_max = math.ceil(taille/10)
+    data_poids = TPoids.objects.all()[t_min:t_max]
+    context = {
+        "Poids" : data_poids,
+        "Index" : idx,
+        "Index_max" : idx_max,
+    }
+    return render(request, 'poids.html', context)
+
+def ajoutPoids(request, idx, valmin, valtimbre):
+    ajoutPoids = TPoids(valmin = valmin, valtimbre = valtimbre)
+    ajoutPoids.save()
+    return HttpResponseRedirect("/pagePoids/"+idx)
+
+def modifierPoids(request, idx, id, valmin, valtimbre):
+    if (valmin != ""):
+        TPoidsv.objects.filter(idpoids=id).update(valmin=valmin)
+    if (valtimbre != ""):
+        TPoidsv.objects.filter(idpoids=id).update(valtimbre=valtimbre)
+    return HttpResponseRedirect("/pagePoids/"+idx)
+
+def pagePoidsVignettes(request, idx):
+    idx = int(idx)
+    t_min = (idx-1)*10
+    t_max = (idx)*10
+    taille = len(TPoidsv.objects.all())
+    idx_max = math.ceil(taille/10)
+    data_poids = TPoidsv.objects.all()[t_min:t_max]
+    context = {
+        "Poids" : data_poids,
+        "Index" : idx,
+        "Index_max" : idx_max,
+    }
+    return render(request, 'poidsVignettes.html', context)
+
+def ajoutPoidsVignettes(request, idx, valmin, valtimbre):
+    ajoutPoids = TPoidsv(valmin = valmin, valtimbre = valtimbre)
+    ajoutPoids.save()
+    return HttpResponseRedirect("/pagePoidsVignettes/"+idx)
+
+def modifierPoidsVignettes(request, idx, id, valmin, valtimbre):
+    if (valmin != ""):
+        TPoidsv.objects.filter(idpoids=id).update(valmin=valmin)
+    if (valtimbre != ""):
+        TPoidsv.objects.filter(idpoids=id).update(valtimbre=valtimbre)
+    return HttpResponseRedirect("/pagePoidsVignettes/"+idx)
