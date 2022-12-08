a64e2c88a61987242e0f568bf314c5eff803e4f0
Madani Bensikhaled <bensikhaledmadani@gmail.com>
2022-12-08 17:05:51 +0100

    Modification de l'orthographe du form de l'onglet utilisateur

--- projet_fil_rouge_api/administration/forms.py
+++ projet_fil_rouge_api/administration/forms.py
@@ -6,5 +6,11 @@ from authentification.models import TUtilisateur
 class TUtilisateurForm(forms.ModelForm):
     class Meta:
         model = TUtilisateur
-        fields = ['nom_utilisateur', 'prenom_utilisateur',
-                  'code_role', 'is_superuser', 'is_active']
+        fields = ['nom_utilisateur', 'prenom_utilisateur', 'code_role', 'is_superuser', 'is_active']
+        labels = {
+            'nom_utilisateur': ("Nom de l'utilisateur "),
+            'prenom_utilisateur': ("Prénom de l'utilisateur "),
+            'code_role': ("Rôle "),
+            'is_superuser': ("L'utilisateur a le status de 'super-user' "),
+            'is_active': ("L'utilisateur est actif "),
+        }
